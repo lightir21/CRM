f28bf42bddb3fe3d1b89b633dbab75039d5bef7b
meir <lightir21@gmail.com>
2022-06-04 00:57:51 +0300

    Filter style

---
+++ src/components/filter/Filter.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./filter.scss";
+
+const Filter = () => {
+  return (
+    <div className="filter">
+      <button>חודש</button>
+      <button>שם</button>
+      <button>מוצר</button>
+      <button>חברה</button>
+      <button>משימה</button>
+      <button>מסתיים החודש</button>
+      <button>שכיר</button>
+      <button>סכום</button>
+      <button>סטאטוס</button>
+    </div>
+  );
+};
+
+export default Filter;
--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Filter from "../../components/filter/Filter";
 import Navbar from "../../components/navbar/Navbar";
 import "./dashboard.scss";
 
@@ -6,6 +7,7 @@ const Dashboard = () => {
   return (
     <div className="dashboard">
       <Navbar />
+      <Filter />
     </div>
   );
 };
