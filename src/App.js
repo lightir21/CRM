f4fd17b73919f2773b799e1207fd5216b675341d
eitai <eitaimeir@gmail.com>
2022-06-03 11:54:04 +0300

    first installemnt

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
+import './styles/global-styles.scss';
+import './styles/global-variables.scss';
+import './styles/css-reset.scss';
+
 function App() {
-  return <div className="App">CRM</div>;
+  return <div className='App'>CRM</div>;
 }
 
 export default App;
